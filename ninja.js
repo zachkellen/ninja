86c0e29957ecbef0fddc0fa3d0d6faaee4a1a6bb
Zach Kellen <z.kellen12@gmail.com>
2021-08-31 14:38:04 -0500

    Sensei class added

--- ninja.js
+++ ninja.js
@@ -19,7 +19,23 @@ class Ninja {
     }
 }
 
-const ninja1 = new Ninja("Hyabusa");
-ninja1.sayName();
-ninja1.drinkSake();
-ninja1.showStats();
+// const ninja1 = new Ninja("Hyabusa");
+// ninja1.sayName();
+// ninja1.drinkSake();
+// ninja1.showStats();
+
+class Sensei extends Ninja{
+    constructor(name, wisdom = 10) {
+        super(name, 200, 10, 10)
+        this.wisdom = wisdom
+    }
+
+    speakWisdom(){
+        this.drinkSake()
+        console.log("Programming is 10% writing code and 90% udnerstanding why it's not working!")
+    }
+}
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
